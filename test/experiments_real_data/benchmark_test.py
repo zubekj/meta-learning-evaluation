15b45453654f799122557f81655ec88ac3e10d8d
Julian Zubek <zubekj@gmail.com>
2012-11-29 22:04:39 +0100

    Building diversive subsets list passes test.

--- test/experiments_real_data/benchmark_test.py
+++ test/experiments_real_data/benchmark_test.py
@@ -12,11 +12,15 @@ class TestBenchmark(unittest.TestCase):
 
     def test_build_set_list_desc_similarity(self):
         data = Orange.data.Table("test.tab")
-        data1 = data.get_items_ref([0,1])
-        data2 = data.get_items_ref([2,1])
-        data3 = data.get_items_ref([2,3])
+
+        data1 = [data[1], data[2]]
+        data2 = [data[3], data[2]]
+        data3 = [data[3], data[0]]
+        
         l = build_set_list_desc_similarity(data, 0.5)
-        self.assertEqual(l[0][0], data1[0])
+        self.assertEqual(list(l[0]), data1)
+        self.assertEqual(list(l[1]), data2)
+        self.assertEqual(list(l[2]), data3)
 
 if __name__ == '__main__':
     unittest.main()
