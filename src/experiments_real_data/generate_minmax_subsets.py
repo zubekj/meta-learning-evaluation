0bd3e32702d0a99311b801586a79e0fa4fe776fd
Julian Zubek <zubekj@gmail.com>
2012-12-21 14:45:40 +0100

    Sparser generation of min/max subsets.

--- src/experiments_real_data/generate_minmax_subsets.py
+++ src/experiments_real_data/generate_minmax_subsets.py
@@ -1,10 +1,16 @@
 import sys
+import math
 import Orange
 import cPickle
 
 sys.path.append('../')
 
 from utils.cSimilarity import *
+
+LEARN_SUBSETS = [1 - math.log(x, 11) for x in xrange(10, 0, -1)] # Log scale
+
+def generate_sizes(dlen):
+    return range(1, int(LEARN_SUBSETS[0] * dlen)) + [p * dlen for p in LEARN_SUBSETS]
 
 if __name__ == '__main__':
 
@@ -17,8 +23,9 @@ if __name__ == '__main__':
 
     rand = Orange.misc.Random(0)
 
-    min_data = build_min_subsets_list_mc(data, range(len(data)), rand)
-    max_data = build_max_subsets_list_mc(data, range(len(data)), rand)
+    subset_sizes = generate_sizes(len(data))
+    min_data = build_min_subsets_list_mc(data, subset_sizes, rand)
+    max_data = build_max_subsets_list_mc(data, subset_sizes, rand)
 
     data_path = "{0}_minmax_subsets.pkl".format(data_file)
 
