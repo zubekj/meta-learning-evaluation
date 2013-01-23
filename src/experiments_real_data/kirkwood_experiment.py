5d8afed855b65f33944d25373ccd6e2cb3c739e2
Julian Zubek <zubekj@gmail.com>
2013-01-23 16:30:18 +0100

    Experiments with Kirkwood and iris dataset.

--- src/experiments_real_data/kirkwood_experiment.py
+++ src/experiments_real_data/kirkwood_experiment.py
@@ -41,47 +41,63 @@ def gauss_digits(n, k_level):
     jd = JointDistributions(data, kirkwood_level=n+1)
 
     x = np.linspace(0,3,100)
-    e1 = np.array([jd.density(dict(zip(xrange(n), float2digits(xi)))) for xi in x])
+    e1 = np.array([jd.margin_density(dict(zip(xrange(n), float2digits(xi)))) for xi in x])
     e1 = e1 / (np.sum(e1) * 0.01 * 3)
 
     jd.kirkwood_level = k_level
-    e2 = np.array([jd.density(dict(zip(xrange(n), float2digits(xi)))) for xi in x])
+    e2 = np.array([jd.margin_density(dict(zip(xrange(n), float2digits(xi)))) for xi in x])
     e2 = e2 / (np.sum(e2) * 0.01 * 3)
-
 
     plt.plot(x,e2, color="blue")
     plt.plot(x,e1, color="green")
     plt.plot(x,mlab.normpdf(x,mean,sigma)*2, color="red")
-
     plt.show()
+    
 
 def iris_approx():
 
     data = Orange.data.Table('iris')
 
     data = Orange.data.discretization.DiscretizeTable(data,
-                 method=Orange.feature.discretization.EqualWidth(n=10))
+                 method=Orange.feature.discretization.EqualWidth(n=len(data)/15))
 
     jd = JointDistributions(data, 5)
-   
+  
+    jd.kirkwood_level = 5
     distinct_values = set(tuple((i, float(d[i])) for i in xrange(4)) for d in data)
-
-    e1 = [jd.density(dict(e)) for e in distinct_values]
-    e1 = e1 / np.sum(e1)
+    e1 = np.array([jd.margin_density(dict(e)) for e in distinct_values])
+    e1 = e1 / np.mean(e1)
     jd.kirkwood_level = 4
-    e2 = [jd.density(dict(e)) for e in distinct_values]
-    e2 = e2 / np.sum(e2)
-    jd.kirkwood_level = 3
-    e3 = [jd.density(dict(e)) for e in distinct_values]
-    e3 = e3 / np.sum(e3)
-    jd.kirkwood_level = 2
-    e4 = [jd.density(dict(e)) for e in distinct_values]
-    e4 = e4 / np.sum(e4)
-
+    e2 = np.array([jd.margin_density(dict(e)) for e in distinct_values])
+    e2 = e2 / np.mean(e2)
+   
+    plt.subplot(311)
     plt.plot(range(len(distinct_values)), e1, color="green")
     plt.plot(range(len(distinct_values)), e2, color="blue")
-    #plt.plot(range(len(distinct_values)), e3, color="red")
-    #plt.plot(range(len(distinct_values)), e4, color="orange")
+
+    jd.kirkwood_level = 5
+    distinct_values = set(tuple((i, float(d[i])) for i in xrange(3)) for d in data)
+    e1 = np.array([jd.margin_density(dict(e)) for e in distinct_values])
+    e1 = e1 / np.mean(e1)
+    jd.kirkwood_level = 3
+    e2 = np.array([jd.margin_density(dict(e)) for e in distinct_values])
+    e2 = e2 / np.mean(e2)
+   
+    plt.subplot(312)
+    plt.plot(range(len(distinct_values)), e1, color="green")
+    plt.plot(range(len(distinct_values)), e2, color="blue")
+
+    jd.kirkwood_level = 5
+    distinct_values = set(tuple((i, float(d[i])) for i in xrange(2)) for d in data)
+    e1 = np.array([jd.margin_density(dict(e)) for e in distinct_values])
+    e1 = e1 / np.mean(e1)
+    jd.kirkwood_level = 2
+    e2 = np.array([jd.margin_density(dict(e)) for e in distinct_values])
+    e2 = e2 / np.mean(e2)
+   
+    plt.subplot(313)
+    plt.plot(range(len(distinct_values)), e1, color="green")
+    plt.plot(range(len(distinct_values)), e2, color="blue")
 
     plt.show()
 
